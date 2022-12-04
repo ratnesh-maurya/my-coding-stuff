43e75719654f572c967339a87b58c4ffeb9466e5
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2022-12-04 22:08:15 +0530

    Create template.cpp

---
+++ codeforces.cpp/template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define I cin >>
+#define O cout <<
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+using namespace std;
+
+
+  
+void solve()
+{
+    
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast
+        ll tc;
+    I tc;
+    while (tc--)
+    {
+        solve();
+        O endl;
+    }
+    return 0;
+}
