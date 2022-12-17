9021c558a19f80ce111d15fc84368f9ae9063fcb
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2022-12-17 20:20:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Break a number/break-a-number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    int waysToBreakNumber(int N){
+        // code here 
+          long M=1000000007;
+
+        long n=N;
+
+        
+
+        return (int)((((n+1)*(n+2)/2))%M);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
