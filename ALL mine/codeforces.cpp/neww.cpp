857b5a51dd01372662d69928f686307e6b6ed893
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2023-03-01 21:15:40 +0530

    Update neww.cpp

--- ALL mine/codeforces.cpp/neww.cpp
+++ ALL mine/codeforces.cpp/neww.cpp
@@ -1,28 +1,14 @@
+/*
+  file-name 
+  code by @ratnesh maurya
+  date
+*/
 #include<bits/stdc++.h>
 using namespace std;
-void printF(int ind,vector<int> &ds,int arr[],int n)
-{
-	if(ind == n)
-	{
-		for(auto i:ds)
-		{
-		cout<<i<<" ";
-		}
-        
-		cout<<endl;
-		return;
-	}
-
-	ds.push_back(arr[ind]);
-	printF(ind+1,ds,arr,n);
-	ds.pop_back();
-	printF(ind+1,ds,arr,n);
-	
-}
+ 
+ 
 int main()
 {
-	int arr[]={3,2,1};
-	int n=3;
-	vector<int>ds;
-	printF(0,ds,arr,n);
+ 
+	return 0;
 }
