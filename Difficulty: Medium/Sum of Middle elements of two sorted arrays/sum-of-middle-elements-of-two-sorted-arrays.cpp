ab223373a319ac2b31e6be3f7f347b64f59a9140
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2024-08-12 22:36:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+//Position this line where user code will be pasted.
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+       int n2=arr2.size();
+        for(int i=0;i<n2;i++){
+            arr1.push_back(arr2[i]);
+        }
+        int n1=arr1.size();
+        sort(arr1.begin(),arr1.end());
+        if(n1%2==0)
+        return arr1[n1/2] + arr1[(n1-2)/2];
+        else
+        return arr1[n1/2] + arr1[(n1+2)/2];
+        }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function template in C++
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        Solution ob;
+        int res = ob.SumofMiddleElements(arr, brr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
